ab4927c1f43b36f5b81657380552cb670deffb90
n0808 <life.with.chums.119@gmail.com>
2020-10-26 17:16:33 +0900

    追加：ヘッダーにif文記載
    ログイン時にユーザー名を表示させるように設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,10 +3,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  # before_action :authenticate_user!, only: [:new]
   # GET /resource/sign_up
   def new
     @user = User.new
+
   end
 
 end
