689cc79709127b87683f86322a113909f91de09e
n0808 <life.with.chums.119@gmail.com>
2020-10-26 15:34:26 +0900

    Update application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,10 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:user] &&
-      password == Rails.application.credentials[:basic_auth][:pass]
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == Rails.application.credentials[:basic_auth][:user] &&
+  #     password == Rails.application.credentials[:basic_auth][:pass]
+  #   end
+  # end
 end
