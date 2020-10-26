43ff7a8b933eaa199a658a5191df0ec8f64bee06
n0808 <life.with.chums.119@gmail.com>
2020-10-26 15:38:13 +0900

    Update application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
-  before_action :authenticate_user!, only: [:new]
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :basic_auth, if: :production?
+  # before_action :authenticate_user!, only: [:new]
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  # protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
 
-  private
-  def production?
-    Rails.env.production?
-  end
+  # private
+  # def production?
+  #   Rails.env.production?
+  # end
 
   # def basic_auth
   #   authenticate_or_request_with_http_basic do |username, password|
