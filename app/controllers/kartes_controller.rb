c0dbecd8a270aa73f02f4cc4a4c0a13235c4f4b6
n0808 <life.with.chums.119@gmail.com>
2020-10-27 13:17:38 +0900

    追加：名前、性別、施設名
    new.htmlに名前、性別、施設名を追加

--- app/controllers/kartes_controller.rb
+++ app/controllers/kartes_controller.rb
@@ -1,4 +1,19 @@
 class KartesController < ApplicationController
   def index
   end
+
+  def new
+    @karte = Karte.new
+  end
+
+  def create
+    Karte.create(karte_params)
+  end
+
+  private
+
+  def karte_params
+    params.require(:karte).permit(:name, :before_treatment, :after_treatment, :part, :doctor_name, :facility_name, :sex_id, :clinic_id, :instructions_id, :medical_examination_id, :day_id,)
+  end
+
 end
