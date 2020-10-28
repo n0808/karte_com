8afdba2c6051e6c50845a4d5e4b5ea41af6ed95d
n0808 <life.with.chums.119@gmail.com>
2020-10-28 14:36:26 +0900

    削除：config.logger
    config.logger = Logger.new(STDOUT)
     config.assets.quiet = true

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module KarteCom
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.logger = Logger.new(STDOUT)
+    # config.logger = Logger.new(STDOUT)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
