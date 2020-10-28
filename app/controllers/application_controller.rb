3210776b9e350adfbd07038aea5f4fa15864bc10
n0808 <life.with.chums.119@gmail.com>
2020-10-28 14:46:09 +0900

    削除：basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
   before_action :authenticate_user!, only: [:new]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -14,10 +13,4 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:user] &&
-      password == Rails.application.credentials[:basic_auth][:pass]
-    end
-  end
 end
