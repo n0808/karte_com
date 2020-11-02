ab284ae0a4ac9ceac190d38ca4f075ca988843a3
n0808 <life.with.chums.119@gmail.com>
2020-11-02 16:24:12 +0900

    追加：時間を日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module KarteCom
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'model.ja.{yml}').to_s]
+    #time_zoneを東京に設定
+    config.time_zone = 'Tokyo'
   end
 end
 
